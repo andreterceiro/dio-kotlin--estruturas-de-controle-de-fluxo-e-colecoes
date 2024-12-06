2e5743c2b005bf547ce64fef4310cc8292c3f933
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-06 13:39:29 -0300

    feat: testingthe filter() method

---
+++ tests/collection-functions.kt
@@ -0,0 +1,26 @@
+package tests
+
+fun main() {
+    testFilter()
+}
+
+fun testFilter() {
+    val mySet: Set<Int> = setOf(1, 2, 3, 4)
+
+    // filter() is not only applied to a set, ok?
+
+    print(
+        mySet.filter({
+            x -> x < 4
+        })
+    )
+
+    print(" -- ")
+
+    // More short, using the keyword 'it'
+    print(
+        mySet.filter({
+            it > 2
+        })
+    )
+}
