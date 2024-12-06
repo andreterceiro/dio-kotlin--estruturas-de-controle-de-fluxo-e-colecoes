2e85ca76df3ebd32c8e8e7b03ec952ec8da70377
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-06 12:14:48 -0300

    feat: adding a test of a mutable list in list.kt

--- tests/list.kt
+++ tests/list.kt
@@ -1,11 +1,23 @@
 package tests
 
 fun main() {
-    var someList: List<Int> = listOf(50,1,3)
-    print(someList)
+    val someList1: List<Int> = listOf(50,1,3)
+    print(someList1)
     print(" - ")
-    print(someList[0])
-    print(" - ") 
-    // sort(someList) // ERROR
-    print(someList)
+    print(someList1[0])
+    print(" - ")
+
+    // someList1[0] = 1 // ERROR - You need a mutable list
+    // sort(someList) // ERROR - intArray as example can be sorted with this function
+    // print(someList1)
+
+    val someList2: MutableList<Int> = mutableListOf(50,1,3)
+    print(someList2)
+    print(" - ")
+    print(someList2[0])
+    print(" - ")
+
+    someList2[0] = 1
+    print(someList2)
+
 }
