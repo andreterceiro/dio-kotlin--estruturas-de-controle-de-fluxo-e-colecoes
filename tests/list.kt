0edda3d87a3175712e0061421d473b4560cb54a8
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-06 10:00:59 -0300

    feat: list tests related to order

---
+++ tests/list.kt
@@ -0,0 +1,11 @@
+package tests
+
+fun main() {
+    var someList: List<Int> = listOf(50,1,3)
+    print(someList)
+    print(" - ")
+    print(someList[0])
+    print(" - ")
+    sort(someList)
+    print(someList)
+}
