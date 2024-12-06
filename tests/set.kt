6090a3e3cc8fa1e69a9c1dce74585ea35ad23e2e
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-06 10:08:43 -0300

    feat: updating the test of sets

--- tests/set.kt
+++ tests/set.kt
@@ -1,10 +1,10 @@
 package tests
 
 fun main() {
-    var someList: Set<Int> = setOf(50,1,3)
-    print(someList)
+    var someList: Set<Int> = setOf(50, 1, 3, 50)
+    print(someList) // [50 , 1 , 3]
     print(" - ")
     print(someList[0]) // Error - not fatal, ok, but an error printed in the screen
     print(" - ")
-    print(someList)
+    print(someList) // [50 , 1 , 3]
 }
